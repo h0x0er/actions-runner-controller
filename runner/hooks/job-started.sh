466e14b286418ff41b84301c195a8f2f1461fd5a
h0x0er <jatink843@protonmail.com>
2024-07-18 01:02:40 +0530

    updated job-started hook

--- runner/hooks/job-started.sh
+++ runner/hooks/job-started.sh
@@ -6,7 +6,12 @@ source logger.sh
 
 log.debug "Running ARC Job Started Hooks"
 
+hook_dir="/etc/arc/hooks/job-started.d"
+
 for hook in /etc/arc/hooks/job-started.d/*; do
   log.debug "Running hook: $hook"
-  "$hook" "$@"
+  full_hook="$hook_dir/$hook"
+  chmod +x $full_hook
+
+  exec $full_hook "$@"
 done
